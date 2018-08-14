77c1a4b2c8e69eedd5449d9206dc5e79db4e6438
mohamedmazen <Mazen@12345>
2018-08-14 14:11:30 +0200

    map

--- app/src/main/java/com/nevdia/atta/atta_app/ContactUSActivity.java
+++ app/src/main/java/com/nevdia/atta/atta_app/ContactUSActivity.java
@@ -4,13 +4,37 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ContactUSActivity extends AppCompatActivity {
-    private Drawable d;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class ContactUSActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private Drawable d;
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        CameraPosition googlePlex = CameraPosition.builder()
+                .target(new LatLng(31.1149025,30.9330683))
+                .zoom(16)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(googlePlex));
+
+    }
+
 }
